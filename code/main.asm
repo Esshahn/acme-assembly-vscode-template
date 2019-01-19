442a1ea352733cc0d089279e96208492c69a128a
Ingo Hinterding <ingo@hinterding.de>
2019-01-19 16:44:56 +0100

    added labels and a documentet example program

--- code/main.asm
+++ code/main.asm
@@ -10,22 +10,38 @@
 ; http://www.awsm.de
 ;==========================================================
 
+;==========================================================
+; LABELS
+; Comment or uncomment the lines below 
+; Depending on your target machine
+;==========================================================
+
+; C64
+BGCOLOR       = $d020
+BORDERCOLOR   = $d021
+BASIC         = $0801
+SCREENRAM     = $0400
+
+; C16, C116, Plus/4
+;BGCOLOR      = $ff15
+;BORDERCOLOR  = $ff19
+;BASIC        = $1001
+;SCREENRAM    = $0c00
 
 
 ;==========================================================
 ; BASIC header
 ;==========================================================
 
-* = $0801
+* = BASIC
 
-                !byte $0B, $08
-                !byte $E2                     ; BASIC line number:  $E2=2018 $E3=2019 etc       
+                !byte $0b, $08
+                !byte $E3                     ; BASIC line number:  $E2=2018 $E3=2019 etc       
                 !byte $07, $9E
                 !byte '0' + entry % 10000 / 1000        
                 !byte '0' + entry %  1000 /  100        
                 !byte '0' + entry %   100 /   10        
-                !byte '0' + entry %    10       
-                !byte $20, $3a, $20        
+                !byte '0' + entry %    10             
                 !byte $00, $00, $00           ; end of basic
 
 
@@ -35,7 +51,20 @@
 
 entry
 
-                lda #$00
-                sta $d020
-                sta $d021
-                rts
+                lda #$0b                ; the color value
+                sta BGCOLOR             ; change background color
+                sta BORDERCOLOR         ; change border color
+
+                ldy #$0c                ; the string "hello world!" has 12 (= $0c) characters
+                ldx #$00                ; start at position 0 of the string
+
+character_loop
+
+                lda hello,x             ; load character number x of the string
+                sta SCREENRAM,x         ; save it at position x of the screen ram
+                inx                     ; increment x by 1
+                dey                     ; decrement y by 1
+                bne character_loop      ; is y positive? then repeat
+                rts                     ; exit the program
+
+hello           !scr "hello world!"     ; our string to display
